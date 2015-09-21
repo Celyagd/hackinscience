e67bed65b241ea4dac52cf5d91b1022d79fba38c
celyagd <celyagd@hackyourphd.org>
2015-09-21 17:11:56 +0200

    exo15

---
+++ exercises/015/solution.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Éditeur de Spyder
+
+Ceci est un script temporaire.
+"""
+import math
+r1 = (245850922 / 78256779)
+print(r1)
+r2 = (1 + math.sqrt(5) / 2)
+print(r2)
+
