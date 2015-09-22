f9d40ba5c56e61221145d28e6eec1b1ca98fd9f4
celyagd <celyagd@hackyourphd.org>
2015-09-22 15:44:49 +0200

    exo60modif

--- exercises/060/solution.py
+++ exercises/060/solution.py
@@ -6,6 +6,6 @@ Ceci est un script temporaire.
 """
 import string
 r = string.ascii_lowercase
-for i in range(0, 26):
-    for n in range(0, 26):
-        print(r[i], r[n])
+for i in r:
+    for n in r:
+        print(i, n)
