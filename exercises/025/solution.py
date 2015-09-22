8e0336d7c4f42d6c5435f092cfeed5fb4caf6203
celyagd <celyagd@hackyourphd.org>
2015-09-22 11:31:29 +0200

    exo25

---
+++ exercises/025/solution.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 10:48:03 2015
+
+@author: celyagruson-daniel
+"""
+import datetime
+date_today = datetime.datetime.today()
+print("Today is", date_today)
