89fe93f9331ec36d82b8d3ea769fc2a2fe9b8fc3
celyagd <celyagd@hackyourphd.org>
2015-09-22 15:10:24 +0200

    exo60

---
+++ exercises/060/solution.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Éditeur de Spyder
+
+Ceci est un script temporaire.
+"""
+import string
+r = string.ascii_lowercase
+for i in range(0, 26):
+    for n in range(0, 26):
+        print(r[i], r[n])
