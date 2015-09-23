5359737c0fdea2f31201e8ec2b398f4eb35a3752
celyagd <celyagd@hackyourphd.org>
2015-09-23 14:49:28 +0200

    exo097

---
+++ exercises/097/solution.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 23 10:30:52 2015
+
+@author: celyagruson-daniel
+"""
+
+
+def love_meet(alice, bob):
+    a = set(alice)
+    b = set(bob)
+    return a.intersection(b)
+
+
+def affair_meet(alice, bob, silvester):
+    a = set(alice)
+    b = set(bob)
+    s = set(silvester)
+    risk_love = set(a.intersection(s))
+    return risk_love.difference(b)
