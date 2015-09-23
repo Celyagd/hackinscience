7167251ee1157bce0fc6a9e9bbb09780a20b9963
celyagd <celyagd@hackyourphd.org>
2015-09-23 08:40:29 +0200

    exo80

--- exercises/080/solution.py
+++ exercises/080/solution.py
@@ -5,9 +5,7 @@
 Ceci est un script temporaire.
 """
 import string
-r = string.ascii_lowercase
-r = "abcde"
-for i in r:
-    for n in r:
-        if (i+n) != (i+n):
-            print(i+n)
+r = list(string.ascii_lowercase)
+for i, item in enumerate(r):
+    for n in r[i + 1:]:
+        print(item + n)
