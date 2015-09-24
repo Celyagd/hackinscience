3da2e565d31ffe4c16991028ffed81b31de2ea21
celyagd <celyagd@hackyourphd.org>
2015-09-24 17:46:31 +0200

    exo107

--- exercises/107/solution.py
+++ exercises/107/solution.py
@@ -4,10 +4,10 @@ Created on Thu Sep 24 16:30:18 2015
 
 @author: celyagruson-daniel
 """
-from operator import itemgetter
 
 
 def select_student(my_class, mark_ok):
+    from operator import itemgetter
     accepted = []
     refused = []
     my_class_sort = sorted(my_class, key=itemgetter(1))
@@ -16,12 +16,4 @@ def select_student(my_class, mark_ok):
             accepted.append(i)
         else:
             refused.append(i)
-
     return {'Accepted': accepted[::-1], 'Refused': refused}
-
-
-
-
-        
-        
-    
