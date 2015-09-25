bcdf3a548b78272ac73395d7b8ec86a266c1626d
celyagd <celyagd@hackyourphd.org>
2015-09-25 22:07:47 +0200

    exo 320

---
+++ exercises/320/solution.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 25 17:03:11 2015
+
+@author: celyagruson-daniel
+"""
+import string
+import re
+
+s = ""
+with open('words') as f:
+    s += f.read()
+
+s = s.replace("\n","").replace("\'","")
+
+for l in string.ascii_lowercase:
+    a = re.findall(l,s)
+    print("{}: {:.2f}".format(l,float(len(a))/len(s)))
