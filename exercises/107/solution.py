8d61665e4a5b1c7b3f6cfd76205dca17b3d255b5
celyagd <celyagd@hackyourphd.org>
2015-09-25 10:17:38 +0200

    exo107

--- exercises/107/solution.py
+++ exercises/107/solution.py
@@ -1,12 +1,19 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Sep 24 16:30:18 2015
+Created on Fri Sep 25 10:02:45 2015
 
 @author: celyagruson-daniel
 """
+
 from operator import itemgetter
 
+my_class = [['Kermit Wade', 27],
+            ['Hattie Schleusner', 67],
+            ['Ben Ball', 5],
+            ['Cen Ball', 5],
+            ['William Lee', 2]]
 
+            
 def select_student(my_class, mark_ok):
     accepted = []
     refused = []
