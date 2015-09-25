2111077d07fe0a217da3a7255ad5acb107796645
celyagd <celyagd@hackyourphd.org>
2015-09-25 14:03:18 +0200

    exo 280

---
+++ exercises/280/solution.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 25 10:02:45 2015
+
+@author: celyagruson-daniel
+"""
+
+import sys
+
+try:
+    print(sys.argv[1])
+except:
+    print("Not enough parameters.")
